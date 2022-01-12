7ea1b7a9e26f6d5e46e07c4884cedf0101bdb2bd
Nanai10a <Nanai10a@outlook.jp>
2022-01-13 00:27:19 +0900

    ♻️ rewritten `link.sh` with lua.

---
+++ link.lua
@@ -0,0 +1,43 @@
+-- import libraries
+
+local lfs = require("lfs")
+
+-- declare utilities
+
+-- name: Execute and returns Void
+local ev = function (cmd)
+    os.execute(cmd)
+end
+
+-- name: Execute and returns Output
+-- from: https://stackoverflow.com/a/326715
+local eo = function (cmd)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+
+  return s
+end
+
+-- name: LiNk Hardly
+local lnh = function (from, to) lfs.link(from, to) end
+
+-- name: LiNk Symbolically
+local lns = function (from, to) lfs.link(from, to, true) end
+
+local is_root = eo("whoami") == "root"
+local pwd = lfs.currentdir()
+local config_dir = os.getenv("HOME") .. "/.config"
+
+-- main program
+
+lns(pwd .. "/dot_config/nvim", config_dir .. "nvim")
+lns(pwd .. "/dot_config/zsh", config_dir .. "zsh")
+
+if (is_root) then
+    lnh(pwd .. "/root/etc/zsh/zshenv", "/etc/zsh/zshenv")
+end

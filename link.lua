aca3f01a75872d732af2cd388c6ec07474adbb70
Nanai10a <Nanai10a@outlook.jp>
2022-01-17 06:18:10 +0900

    🎨 formatted.

--- link.lua
+++ link.lua
@@ -5,29 +5,33 @@ local lfs = require("lfs")
 -- declare utilities
 
 -- name: Execute and returns Void
-local ev = function (cmd)
+local ev = function(cmd)
     os.execute(cmd)
 end
 
 -- name: Execute and returns Output
 -- from: https://stackoverflow.com/a/326715
-local eo = function (cmd)
-  local f = assert(io.popen(cmd, 'r'))
-  local s = assert(f:read('*a'))
-  f:close()
+local eo = function(cmd)
+    local f = assert(io.popen(cmd, "r"))
+    local s = assert(f:read("*a"))
+    f:close()
 
-  s = string.gsub(s, '^%s+', '')
-  s = string.gsub(s, '%s+$', '')
-  s = string.gsub(s, '[\n\r]+', ' ')
+    s = string.gsub(s, "^%s+", "")
+    s = string.gsub(s, "%s+$", "")
+    s = string.gsub(s, "[\n\r]+", " ")
 
-  return s
+    return s
 end
 
 -- name: LiNk Hardly
-local lnh = function (from, to) lfs.link(from, to) end
+local lnh = function(from, to)
+    lfs.link(from, to)
+end
 
 -- name: LiNk Symbolically
-local lns = function (from, to) lfs.link(from, to, true) end
+local lns = function(from, to)
+    lfs.link(from, to, true)
+end
 
 local is_root = eo("whoami") == "root"
 local pwd = lfs.currentdir()
@@ -38,6 +42,6 @@ local config_dir = os.getenv("XDG_CONFIG_HOME")
 lns(pwd .. "/dot_config/nvim", config_dir .. "/nvim")
 lns(pwd .. "/dot_config/zsh", config_dir .. "/zsh")
 
-if (is_root) then
+if is_root then
     lnh(pwd .. "/root/etc/zsh/zshenv", "/etc/zsh/zshenv")
 end

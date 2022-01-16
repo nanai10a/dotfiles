4ed3bbc26baa1b36a655c7bc142c80cc223d4f64
Nanai10a <Nanai10a@outlook.jp>
2022-01-17 06:28:37 +0900

    🚧 add alacritty configuration.

--- link.lua
+++ link.lua
@@ -42,6 +42,7 @@ local config_dir = os.getenv("XDG_CONFIG_HOME")
 lns(pwd .. "/dot_config/nvim", config_dir .. "/nvim")
 lns(pwd .. "/dot_config/zsh", config_dir .. "/zsh")
 lns(pwd .. "/dot_config/stylua", config_dir .. "/stylua")
+lns(pwd .. "/dot_config/alacritty", config_dir .. "/alacritty")
 
 if is_root then
     lnh(pwd .. "/root/etc/zsh/zshenv", "/etc/zsh/zshenv")

4b4d82f1414c59b387ed52e9e04a117e14d1ca94
Nanai10a <Nanai10a@outlook.jp>
2022-01-17 00:27:36 +0900

    ➕ add `nvim-ts-autotag` and `nvim-ts-rainbow` (disabled).

--- dot_config/nvim/lua/dein/install/treesitter.lua
+++ dot_config/nvim/lua/dein/install/treesitter.lua
@@ -1,1 +1,7 @@
+-- base plugin
 vim.fn["dein#add"]("nvim-treesitter/nvim-treesitter")
+
+-- extension plugins
+vim.fn["dein#add"]("windwp/nvim-ts-autotag")
+-- disabled `nvim-ts-rainbow`: *not good* and maybe too late (> 900ms !?)
+-- vim.fn["dein#add"]("p00f/nvim-ts-rainbow")
--- dot_config/nvim/lua/dein/settings/treesitter.lua
+++ dot_config/nvim/lua/dein/settings/treesitter.lua
@@ -1,7 +1,29 @@
+-- local color_suffixes = {
+--     "8", "9", "A", "B", "C", "D", "E", "F"
+-- }
+-- local colors = {}
+-- local termcolors = {}
+--
+-- for _, s in ipairs(color_suffixes) do
+--     local color = "#" .. vim.g["base16_gui0" .. s]
+--     local termcolor = vim.g["base16_cterm0" .. s]
+--     table.insert(colors, color)
+--     table.insert(termcolors, termcolor)
+-- end
+
 require("nvim-treesitter.configs").setup({
     ensure_installed = "maintained",
     sync_install = false,
     highlight = {
         enable = true,
-    }
+    },
+    -- rainbow = {
+    --     enable = true,
+    --     extended_mode = true,
+    --     max_file_lines = nil,
+    --     colors = colors,
+    --     termcolors = termcolors,
+    -- },
 })
+
+require("nvim-ts-autotag").setup()

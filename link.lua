bda10339218c9236b9ba276d8e640aae74a41aab
Nanai10a <Nanai10a@outlook.jp>
2022-01-25 16:04:15 +0900

    🚧 add swaylock configuration.

--- link.lua
+++ link.lua
@@ -45,6 +45,7 @@ lns(pwd .. "/dot_config/stylua", config_dir .. "/stylua")
 lns(pwd .. "/dot_config/alacritty", config_dir .. "/alacritty")
 lns(pwd .. "/dot_config/sway", config_dir .. "/sway")
 lns(pwd .. "/dot_config/waybar", config_dir .. "/waybar")
+lns(pwd .. "/dot_config/swaylock", config_dir .. "/swaylock")
 
 if is_root then
     lnh(pwd .. "/root/etc/zsh/zshenv", "/etc/zsh/zshenv")

f8704c9e5a1269696fcf00583bf6b91264dc8076
Nanai Jua <me@n7i.dev>
2022-04-16 10:58:43 +0900

    🚧 (nvim) add logging on `lua/python` .

--- dot_config/nvim/lua/python.lua
+++ dot_config/nvim/lua/python.lua
@@ -1,9 +1,18 @@
+local lprint = function(s)
+    print("[c:python] " .. s)
+end
+
 local install = function(mod)
     os.execute("pip3 install --user " .. mod .. " &> /dev/null &")
 end
 
 if vim.fn.has("python3") ~= 1 then
+    lprint("python3 does not recognized.")
+    lprint("starting to install requiring modules with pip3...")
+
     install("pynvim")
     install("send2trash")
     install("ueberzug")
+
+    lprint("installed python3 modules.")
 end

20036a3cf4d84a798dff66cff99703e2f8f65755
Nanai Jua <me@n7i.dev>
2022-05-24 07:15:36 +0900

    ➕ (nvim) re:add `nvim-ts-rainbow` .

--- dot_config/nvim/lua/dein/install/treesitter.lua
+++ dot_config/nvim/lua/dein/install/treesitter.lua
@@ -3,5 +3,4 @@ vim.fn["dein#add"]("nvim-treesitter/nvim-treesitter")
 
 -- extension plugins
 vim.fn["dein#add"]("windwp/nvim-ts-autotag")
--- disabled `nvim-ts-rainbow`: *not good* and maybe too late (> 900ms !?)
--- vim.fn["dein#add"]("p00f/nvim-ts-rainbow")
+vim.fn["dein#add"]("p00f/nvim-ts-rainbow")
--- dot_config/nvim/lua/dein/settings/treesitter.lua
+++ dot_config/nvim/lua/dein/settings/treesitter.lua
@@ -4,11 +4,11 @@ require("nvim-treesitter.configs").setup({
     highlight = {
         enable = true,
     },
-    -- rainbow = {
-    --     enable = true,
-    --     extended_mode = true,
-    --     max_file_lines = nil,
-    -- },
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+    },
 })
 
 require("nvim-ts-autotag").setup()

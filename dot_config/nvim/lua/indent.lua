b84d95c1eb7595d95b997574bde1b0123c16857a
Nanai Jua <me@n7i.dev>
2022-05-24 07:10:38 +0900

    🚧 (nvim) add 2-space-indent filetypes.

--- dot_config/nvim/lua/indent.lua
+++ dot_config/nvim/lua/indent.lua
@@ -1,4 +1,4 @@
 vim.o.shiftwidth = 4
 
 vim.cmd("au BufRead,BufNewFile *.scss set filetype=scss.css")
-vim.cmd("au BufRead,BufNewFile *.{js,jsx,ts,tsx,css,scss} set shiftwidth=2")
+vim.cmd("au BufRead,BufNewFile *.{js,jsx,ts,tsx,css,scss,json,yaml,html} set shiftwidth=2")

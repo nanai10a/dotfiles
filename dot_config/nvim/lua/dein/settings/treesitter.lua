0f9227154b2798f37804b9ba05c5a9383f1759c8
Nanai Jua <me@n7i.dev>
2022-05-24 07:13:39 +0900

    🐛 🔥 (nvim) delete unworking codes.

--- dot_config/nvim/lua/dein/settings/treesitter.lua
+++ dot_config/nvim/lua/dein/settings/treesitter.lua
@@ -1,23 +1,3 @@
--- local color_suffixes = {
---     "8",
---     "9",
---     "A",
---     "B",
---     "C",
---     "D",
---     "E",
---     "F",
--- }
--- local colors = {}
--- local termcolors = {}
---
--- for _, s in ipairs(color_suffixes) do
---     local color = "#" .. vim.g["base16_gui0" .. s]
---     local termcolor = vim.g["base16_cterm0" .. s]
---     table.insert(colors, color)
---     table.insert(termcolors, termcolor)
--- end
-
 require("nvim-treesitter.configs").setup({
     ensure_installed = "all",
     sync_install = false,
@@ -28,8 +8,6 @@ require("nvim-treesitter.configs").setup({
     --     enable = true,
     --     extended_mode = true,
     --     max_file_lines = nil,
-    --     colors = colors,
-    --     termcolors = termcolors,
     -- },
 })
 

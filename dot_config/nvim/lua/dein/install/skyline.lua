ca59547c2937a7c655efd7d91679616cabb54ad5
Nanai Jua <me@n7i.dev>
2022-05-28 15:56:05 +0900

    ➕ (nvim) add `skyline.vim` (replacing `lualine.nvim` ).

---
+++ dot_config/nvim/lua/dein/install/skyline.lua
@@ -0,0 +1,6 @@
+-- base plugin
+vim.fn["dein#add"]("ourigen/skyline.vim")
+
+-- dependency plugins
+-- vim.fn["dein#add"]("dense-analysis/ale")
+-- vim.fn["dein#add"]("tpope/vim-fugitive")
---
+++ dot_config/nvim/lua/dein/settings/skyline.lua
@@ -0,0 +1,5 @@
+-- FIXME: not working?
+-- vim.g.skyline_ale = 1
+
+-- FIXME: broken
+-- vim.g.skyline_fugitive = 1

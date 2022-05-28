1536e750b0c72aa08eaf313dbb08725b461112b1
Nanai Jua <me@n7i.dev>
2022-05-28 16:06:10 +0900

    ➖ (nvim) remove `nvim-ts-rainbow` .

--- dot_config/nvim/lua/dein/install/treesitter.lua
+++ dot_config/nvim/lua/dein/install/treesitter.lua
@@ -3,4 +3,3 @@ vim.fn["dein#add"]("nvim-treesitter/nvim-treesitter")
 
 -- extension plugins
 vim.fn["dein#add"]("windwp/nvim-ts-autotag")
-vim.fn["dein#add"]("p00f/nvim-ts-rainbow")
--- dot_config/nvim/lua/dein/settings/treesitter.lua
+++ dot_config/nvim/lua/dein/settings/treesitter.lua
@@ -3,11 +3,6 @@ require("nvim-treesitter.configs").setup({
     highlight = {
         enable = true,
     },
-    rainbow = {
-        enable = true,
-        extended_mode = true,
-        max_file_lines = nil,
-    },
 })
 
 require("nvim-ts-autotag").setup()

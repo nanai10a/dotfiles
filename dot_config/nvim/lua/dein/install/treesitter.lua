8518970adf50cf94d716cad57b720f00d7a91412
Nanai Jua <me@n7i.dev>
2022-05-28 16:07:51 +0900

    🐛 (nvim) fix treesitter rebuilds on each update plugins.

--- dot_config/nvim/lua/dein/install/treesitter.lua
+++ dot_config/nvim/lua/dein/install/treesitter.lua
@@ -1,5 +1,5 @@
 -- base plugin
-vim.fn["dein#add"]("nvim-treesitter/nvim-treesitter")
+vim.fn["dein#add"]("nvim-treesitter/nvim-treesitter", { hook_post_update = "TSUpdate" })
 
 -- extension plugins
 vim.fn["dein#add"]("windwp/nvim-ts-autotag")

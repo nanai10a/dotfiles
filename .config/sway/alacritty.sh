cc40c8bfb155924e66cd10af34760839f09be05a
Nanai Jua <me@n7i.dev>
2024-01-11 18:58:55 +0900

    fix(sway): it's not `$XDG_CONFIG_DIR`, `$XDG_RUNTIME_DIR` is correct

--- .config/sway/alacritty.sh
+++ .config/sway/alacritty.sh
@@ -1,1 +1,1 @@
-find $XDG_CONFIG_DIR -maxdepth 1 -type s -name 'Alacritty-"*-*-*.sock' -exec /bin/sh -c 'exit 1' {} + -quit && alacritty msg create-window || alacritty
+find $XDG_RUNTIME_DIR -maxdepth 1 -type s -name 'Alacritty-"*-*-*.sock' -exec /bin/sh -c 'exit 1' {} + -quit && alacritty msg create-window || alacritty
